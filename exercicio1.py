284266016aa035eb72292ee80d68a14c2359c7fe
Cibely Pereira <cibelypereira16@gmail.com>
2019-08-19 16:40:28 -0300

    Adicionar exercicios

---
+++ exercicio1.py
@@ -0,0 +1,7 @@
+lista = ['2','5','7','10','13','18']
+
+for lista in listas
+    if valores /2 == 0
+        return print('Os números pares são: {valores}')
+    else
+        return print ('Os números impares são: {valores}')
---
+++ exercicio2.py
@@ -0,0 +1,6 @@
+numero = input('Digite 9 números: ')
+    
+if numero[0] == '9' and len(numero) =='9':
+    print('Número válido')
+else:
+    print('Número inválido')
